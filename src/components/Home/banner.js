f2fed34f1f298721c6b8a4ade5ff6f257e14cefb
abhilash6398 <abhilash6398@gmail.com>
2021-10-18 16:17:58 +0530

    updated-addAddress

--- src/components/Home/banner.js
+++ src/components/Home/banner.js
@@ -1,65 +1,38 @@
-import React, { Component, useCallback, useEffect } from "react";
-import axios from "./Axios";
-// import "../../style/banner.css";
+import React, { Component } from "react";
+// import "../../style/Mainn.css";
 // import "../assets/css/style.css";
 
-export default class Banner extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      Banner: [],
+class Mainn extends Component {
+  constructor(){
+    super();
+    this.state={
+      Mainn: []
     };
   }
-  getBannersData() {
-    axios
-      .get(`posterPage`, {})
-      .then((res) => {
-        const data = res.data;
-        console.log(data);
-        console.log(data.data[0].image[0].img);
-        const banners = data.data.map((data) => (
-          <div className="container-fluid">
-            <div class="shopbyintrest">
-              <div class="titleviewrow">
-                {/* <div class="titleview">
-                  <span>posterPage</span>
-                </div> */}
-              </div>
-              <div class="shopviewint">
-                <div
-                  id="latestproducts"
-                  class="owl-carousel latestproducts owl-theme"
-                >
-                  <div class="item clearfix">
-                    <div class="productwtview">
-                      <div class="imgview">
-                        <img src={data.image.img} alt="" style={{height:"300px",width:"600px"}} />
-                      </div>
-                      {/* <span>{data.description}</span> */}
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        ));
 
-        this.setState({
-          banners,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+  componentDidMount(){
+    fetch("/posterPage")
+    .then(response=>{
+      if(!response.ok){
+        throw Error("Error fetching")
+      }
+      return response.json()
+    })
+    .then(allData=>{
+      this.setState({Mainn: allData});
+    })
+    .catch(err=>{
+      throw Error (err.message);
+    })
   }
 
-  componentDidMount() {
-    this.getBannersData();
-  }
-  render() {
-    return <>{this.state.banners}
-    <banners Banner={Banner}/>
-
-    </>;
+  render(){
+    return(
+      <section className="xyz">
+        <p>Hello</p>
+      </section>
+    )
   }
 }
+
+export default Mainn;
