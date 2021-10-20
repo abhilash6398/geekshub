d77a8bd12948ee8601c5e713277498165529d963
abhilash6398 <abhilash6398@gmail.com>
2021-10-20 10:51:44 +0530

    updated

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,91 +1,100 @@
-import React from "react";
+import React,{useState} from "react";
 import "../style/Login.css";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 const Reset = () => {
-  const initialValues = {
-    password: "",
-    confirmPassword: "",
-  };
+  const [email, setEmail] = useState("");
+  const [otp, setOtp] = useState("");
+  const [newpassword, setNewPassword] = useState("");
 
-  const validationSchema = Yup.object().shape({
-    password: Yup.string().required("*Required"),
-    confirmPassword: Yup.string().required("*Required"),
-  });
-
-  const onSubmit = (event) => {
-    const password = event.target.password.value;
-    const confirmPassword = event.target.confirmPassword.value;
-    const data = {
-      password,
-      confirmPassword,
-    };
-
-    axios
-      .post("http://3.109.247.241:6778/api/v2/customer/resetPassword", data)
-      .then((response) => {
-        console.log(response);
-        event.target.reset();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  //   const onSubmit = (values, props) => {
-  //     console.log(values);
-
-  //     setTimeout(() => {
-  //       props.resetForm();
-  //       props.setSubmitting(false);
-  //     }, 2000);
-  //   };
+  function saveChanges() {
+    console.warn({email,otp,newpassword});
+    let data = {email,otp,newpassword};
+    fetch("http://3.109.247.241:6778/api/v2/customer/resetPassword", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        // token: userToken.userData.data.token,
+      },
+      body: JSON.stringify(data),
+    }).then((result) => {
+      console.warn("result", result);
+    });
+  }
 
   return (
-    <div>
+    <div className="container">
       <div class="loginwrap">
         <div class="pgaddressrow">
-          <h1 class="heading">Login / Register</h1>
+          <h1 class="heading">Reset Password</h1>
           <h4 class="smheading">
             <a href="/home ">Home</a> / Login / <a href="/reset ">Reset</a>
           </h4>
         </div>
-        <div class="loginformCon">
-          <h2>RESET</h2>
-
-          <Formik
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-          >
-            {(props) => (
-              <Form>
-                <div className="loginforminputs">
-                  <div className="form-group">
-                    <label>Enter Your Password</label>
-                    <Field name="password" className="form-control" type="password" />
-                    <div className="error">
-                      <ErrorMessage name="password" />
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <label>Confirm Your Password</label>
-                    <Field name="password" className="form-control" type="password" />
-                    <div className="error">
-                      <ErrorMessage name="confirmPassword" />
-                    </div>
-                  </div>
-                  <div className="loginbtnrow">
-                    <button className="loginbtn" type="submit">
-                      {props.isSubmitting ? "Loading" : "SUBMIT"}
-                    </button>
+        <div class="loginformCon signup">
+        <h4>RESET</h4>
+        <h2>PASSWORD</h2>
+          <div class="loginforminputs">
+            <div class="imvendwrap">
+              <div class="row clearfix">
+                <div class="col-sm-12">
+                  <div class="form-group">
+                    <label>Email</label>
+                    <input
+                      name="email"
+                      class="form-control"
+                      type="email"
+                      placeholder="Enter Your Email Address"
+                      value={email}
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
                   </div>
                 </div>
-              </Form>
-            )}
-          </Formik>
+                <div class="col-sm-12">
+                  <div class="form-group">
+                    <label>Otp</label>
+                    <input
+                      name="Otp"
+                      class="form-control"
+                      type="number"
+                      placeholder="Enter Your Otp"
+                      value={otp}
+                      onChange={(e) => {
+                        setOtp(e.target.value);
+                      }}
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-12">
+                  <div class="form-group">
+                    <label>New Password</label>
+                    <input
+                      name="newpassword"
+                      class="form-control"
+                      type="password"
+                      placeholder="Enter Your New Password"
+                      value={newpassword}
+                      onChange={(e) => {
+                        setNewPassword(e.target.value);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-warning"
+              onClick={saveChanges}
+            >
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </div>
